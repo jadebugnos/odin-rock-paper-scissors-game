0c3cadddde7f20cdf16e16ed47c5a6e2c948ef18
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-08-31 21:35:23 +0800

    feat: add playRound function
    returns a string that determines the winner of the
    round

--- script.js
+++ script.js
@@ -24,6 +24,33 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, robotChoice) {
+    let sum = humanChoice + "." + robotChoice;
+    let winner = "you win! ";
+    let loser = "You lost! ";
+    let message;
+    let str1 = "scissors beats paper!";
+    let str2 = "paper beats rock";
+    let str3 = "rock beats scissors";
+    
+if (sum === scissors.paper) {
+    message = winner + str1;
+} else if (sum === paper.rock) {
+    message = winner + str2;
+} else if (sum === rock.scissors) {
+    message = winner + str3;
+} else if (sum === scissors.rock) {
+    message = loser + str3;
+} else if (sum === paper.scissors) {
+    message = loser + str1;
+} else if (sum === rock.paper) {
+    message = loser + str2;
+}
+    console.log (message);
+    return message;
+}
 
+playRound(getHumanChoice, getComputerChoice);
 getHumanChoice()
 
+// if (sum === scissors.paper || sum === paper.rock || sum === rock.scissors) {
