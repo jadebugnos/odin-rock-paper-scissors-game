e9db8bf819c7f3dd0296916999e32d0d8f56e468
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-01 09:42:59 +0800

    feat: enhance playRound function to update scores
    - Implemented logic to increment the score of the
     player who wins the round

--- script.js
+++ script.js
@@ -38,16 +38,22 @@ function playRound(humanChoice, robotChoice) {
     
 if (sum === "scissors.paper") {
     message = winner + str1;
+    humanScore++
 } else if (sum === "paper.rock") {
     message = winner + str2;
+    humanScore++
 } else if (sum === "rock.scissors") {
     message = winner + str3;
+    humanScore++
 } else if (sum === "scissors.rock") {
     message = loser + str3;
+    computerScore++
 } else if (sum === "paper.scissors") {
     message = loser + str1;
+    computerScore++
 } else if (sum === "rock.paper") {
     message = loser + str2;
+    computerScore++
 } else {
     message = "draw! try again";
 }
@@ -55,14 +61,8 @@ if (sum === "scissors.paper") {
     return message;
 }
 
-// switch (sum) {
-//     case "scissors.paper":
-//         message = winner + str1;
-//         break;
-
-// }
 
 playRound(getHumanChoice(), getComputerChoice());
-
+console.log(humanScore, computerScore);
 
 // if (sum === scissors.paper || sum === paper.rock || sum === rock.scissors) {
