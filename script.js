a1ba1aa3827e93dea485f0d224eebdebf8849dc8
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-02 16:18:24 +0800

    edit: move playRound inside playGame

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
-
-
 function getComputerChoice() {
     if (Math.floor((Math.random() * 3) + 1) === 1) {
         return "rock";
@@ -19,64 +14,56 @@ function getHumanChoice() {
     return newChoice;
 }
 
-function playRound(humanChoice, robotChoice) {
-    let sum = humanChoice + "." + robotChoice;
-    console.log(sum);
-    let winner = "you win! ";
-    let loser = "You lost! ";
-    let message;
-    let str1 = "scissors beats paper!";
-    let str2 = "paper beats rock";
-    let str3 = "rock beats scissors";
-    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
-        if (sum === "scissors.paper") {
-            message = winner + str1;
-            humanScore++
-        } else if (sum === "paper.rock") {
-            message = winner + str2;
-            humanScore++
-        } else if (sum === "rock.scissors") {
-            message = winner + str3;
-            humanScore++
-        } else if (sum === "scissors.rock") {
-            message = loser + str3;
-            computerScore++
-        } else if (sum === "paper.scissors") {
-            message = loser + str1;
-            computerScore++
-        } else if (sum === "rock.paper") {
-            message = loser + str2;
-            computerScore++
-        } else {
-            message = "draw! try again";
-        }
-        console.log(message);
-        return message;
-    } else {
-        alert("You can only pick rock, paper and scissors!");
-    }
-}
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 
-console.log(humanScore, computerScore);
-
-
-function playGame(human, cpu) {
+function playGame() {
     let victory = "Congratulations! You won the game!";
     let defeat = "Game over! you lost the game!";
+    let humanScore = 0;
+    let computerScore = 0;
 
-    while (human < 5 || cpu < 5) {
-        playRound(humanSelection, computerSelection);
-    }
+    function playRound(humanChoice, robotChoice) {
+        let sum = humanChoice + "." + robotChoice;
+        console.log(sum);
+        let winner = "you win! ";
+        let loser = "You lost! ";
+        let message;
+        let str1 = "scissors beats paper!";
+        let str2 = "paper beats rock";
+        let str3 = "rock beats scissors";
 
-    if (human == 5 || cpu == 5) {
-        return victory;
-    } else {
-        return defeat;
+        if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+            if (sum === "scissors.paper") {
+                message = winner + str1;
+                humanScore++
+            } else if (sum === "paper.rock") {
+                message = winner + str2;
+                humanScore++
+            } else if (sum === "rock.scissors") {
+                message = winner + str3;
+                humanScore++
+            } else if (sum === "scissors.rock") {
+                message = loser + str3;
+                computerScore++
+            } else if (sum === "paper.scissors") {
+                message = loser + str1;
+                computerScore++
+            } else if (sum === "rock.paper") {
+                message = loser + str2;
+                computerScore++
+            } else {
+                message = "draw! try again";
+            }
+            console.log(message);
+            console.log(humanScore, computerScore);
+            return message;
+        } else {
+            alert("You can only pick rock, paper and scissors!");
+        }
     }
 }
 
-console.log(playGame(humanScore, computerScore));
+playGame();
