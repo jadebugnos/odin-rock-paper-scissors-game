46c598f4731b1f19e497b9ebc25adba8ee743b3b
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-02 08:57:03 +0800

    refactor: getHumanChoice and playRound functions
    - Removed if-else statement in getHumanChoice
    - Fixed playRound to execute only if
    getHumanChoice returns valid input

--- script.js
+++ script.js
@@ -16,14 +16,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let choice = prompt("Let's play rock paper scissors, type your choice!");
     newChoice = choice.toLowerCase();
-    if (newChoice == "rock" || newChoice == "paper" || newChoice == "scissors") {
-        console.log(newChoice);
-        return newChoice;
-    } 
-    else {
-         alert("I'm sorry you can only choose rock, paper or scissors");
-         getHumanChoice();
-    }
+    return newChoice;
 }
 
 function playRound(humanChoice, robotChoice) {
@@ -35,34 +28,38 @@ function playRound(humanChoice, robotChoice) {
     let str1 = "scissors beats paper!";
     let str2 = "paper beats rock";
     let str3 = "rock beats scissors";
-    
-if (sum === "scissors.paper") {
-    message = winner + str1;
-    humanScore++
-} else if (sum === "paper.rock") {
-    message = winner + str2;
-    humanScore++
-} else if (sum === "rock.scissors") {
-    message = winner + str3;
-    humanScore++
-} else if (sum === "scissors.rock") {
-    message = loser + str3;
-    computerScore++
-} else if (sum === "paper.scissors") {
-    message = loser + str1;
-    computerScore++
-} else if (sum === "rock.paper") {
-    message = loser + str2;
-    computerScore++
-} else {
-    message = "draw! try again";
-}
-    console.log (message);
-    return message;
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+        if (sum === "scissors.paper") {
+            message = winner + str1;
+            humanScore++
+        } else if (sum === "paper.rock") {
+            message = winner + str2;
+            humanScore++
+        } else if (sum === "rock.scissors") {
+            message = winner + str3;
+            humanScore++
+        } else if (sum === "scissors.rock") {
+            message = loser + str3;
+            computerScore++
+        } else if (sum === "paper.scissors") {
+            message = loser + str1;
+            computerScore++
+        } else if (sum === "rock.paper") {
+            message = loser + str2;
+            computerScore++
+        } else {
+            message = "draw! try again";
+        }
+        console.log(message);
+        return message;
+    } else {
+        alert("You can only pick rock, paper and scissors!");
+    }
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-playRound(getHumanChoice(), getComputerChoice());
+playRound(humanSelection, computerSelection);
 console.log(humanScore, computerScore);
 
-// if (sum === scissors.paper || sum === paper.rock || sum === rock.scissors) {
