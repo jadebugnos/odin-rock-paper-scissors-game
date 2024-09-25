11cee746512e250fcd62515d93004b111c5433d5
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-25 21:19:46 +0800

    refactor playgame function and eventlistner
    - Hide unchosen computer buttons when a button is
    clicked.
    - Separate playground function for new UI game
    logic.

--- script.js
+++ script.js
@@ -11,69 +11,52 @@ function getComputerChoice() {
 }
 
 
-//commented out incase I'll need need later
-// function getHumanChoice() {
-//     let choice = prompt("Let's play rock paper scissors, type your choice!");
+function playRound(humanChoice, robotChoice) {
+    let sum = humanChoice + " vs " + robotChoice;
+    console.log(sum);
+    let winner = "you win! ";
+    let loser = "You lost! ";
+    let message;
+    let str1 = "scissors beats paper!";
+    let str2 = "paper beats rock";
+    let str3 = "rock beats scissors";
 
-//     if (!choice) {
-//         return choice = "unknown";
-//     } else {
-//         return choice.toLowerCase();
-//     }
-// }
-
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+        if (sum === "scissors.paper") {
+            message = winner + str1;
+            humanScore++
+        } else if (sum === "paper vs rock") {
+            message = winner + str2;
+            humanScore++
+        } else if (sum === "rock vs scissors") {
+            message = winner + str3;
+            humanScore++
+        } else if (sum === "scissors vs rock") {
+            message = loser + str3;
+            computerScore++
+        } else if (sum === "paper vs scissors") {
+            message = loser + str1;
+            computerScore++
+        } else if (sum === "rock vs paper") {
+            message = loser + str2;
+            computerScore++
+        } else {
+            message = "draw! try again";
+        }
+        console.log(message);
+        console.log(humanScore, computerScore);
+    } else {
+        alert("You can only pick rock, paper and scissors!");
+    }
+}
 
 function playGame() {
     let victory = "Congratulations! You won the game! refresh to restart.";
     let defeat = "Game over! you lost the game! refresh to restart.";
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, robotChoice) {
-        let sum = humanChoice + " vs " + robotChoice;
-        console.log(sum);
-        let winner = "you win! ";
-        let loser = "You lost! ";
-        let message;
-        let str1 = "scissors beats paper!";
-        let str2 = "paper beats rock";
-        let str3 = "rock beats scissors";
-
-        if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
-            if (sum === "scissors.paper") {
-                message = winner + str1;
-                humanScore++
-            } else if (sum === "paper vs rock") {
-                message = winner + str2;
-                humanScore++
-            } else if (sum === "rock vs scissors") {
-                message = winner + str3;
-                humanScore++
-            } else if (sum === "scissors vs rock") {
-                message = loser + str3;
-                computerScore++
-            } else if (sum === "paper vs scissors") {
-                message = loser + str1;
-                computerScore++
-            } else if (sum === "rock vs paper") {
-                message = loser + str2;
-                computerScore++
-            } else {
-                message = "draw! try again";
-            }
-            console.log(message);
-            console.log(humanScore, computerScore);
-        } else {
-            alert("You can only pick rock, paper and scissors!");
-        }
-    }
-
-    // const humanSelection = getHumanChoice();
-    // const computerSelection = getComputerChoice(); to be transferred to eventlisterner later
-
-    // playRound(humanSelection, computerSelection);
-
 }
+
+// to be transferred to eventlisterner later
+// playRound(humanSelection, computerSelection);
 
 const robotChoice = document.querySelector('#robot-choices');
 const buttons = document.querySelector('#button-container');
@@ -84,6 +67,9 @@ const counter = document.querySelector('#counter');
 buttons.addEventListener('click', event => {
     let target = event.target;
     const buttons = document.querySelectorAll(".buttons");
+    const computerSelection = getComputerChoice();
+    
+    toHideRobotButtons(computerSelection);
 
     if (event.target.tagName === 'IMG') target = event.target.parentElement;
 
@@ -94,7 +80,7 @@ buttons.addEventListener('click', event => {
     }
 })
 
-//the choice here should be passed with getComputerChoice function
+//should be passed with getComputerChoice function
 function toHideRobotButtons(choice) {
     const robots = document.querySelectorAll('.robot');
 
@@ -104,11 +90,9 @@ function toHideRobotButtons(choice) {
         } else {
             robot.style.display = "block";
         }
-    } 
-
+    }
 }
 
 
 
 
-toHideRobotButtons(getComputerChoice())
